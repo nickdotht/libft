93b141622ffdef39a9bfd396dfd6e89e101d9dc6
Nick Rameau <r4meau@gmail.com>
2017-02-26 17:39:37 -0800

    Fixing commits

---
+++ fix.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+git filter-branch --env-filter '
+OLD_EMAIL="jrameau@e1z2r8p23.42.us.org"
+CORRECT_NAME="Nick Rameau"
+CORRECT_EMAIL="r4meau@gmail.com"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+j
